d6d82e57f8756c220009f4a416b582c138f2d1db
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-07-04 11:05:37 -0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,55 +1,30 @@
-let limit = 30
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
-  }
-  if (yt === false) throw 'Semua server tidak bisa :/'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
-*Server y2mate:* ${usedServer}
+let handler = async (m, { conn, command, text }) => {
+if (!text) throw '*✳️ Inserte el nombre/título del video o audio a bucar*\n\n*Ejemplo:*\n*#play Juan Solo - Querido Corazón*'
+let results = await yts(text)
+let vid = results.all.find(video => video.seconds < 3600)
+if (!vid) throw '*Video/Audio No encontrado* '
+let isVideo = /2$/.test(command)
+let { dl_link, thumb, title, filesize, filesizeF} = await (isVideo ? ytv : yta)(vid.url, 'id4')
+conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
+*${title}*
+*⇄ㅤ     ◁   ㅤ  ❚❚ㅤ     ▷ㅤ     ↻*
+
+*📂Tamaño del archivo:* ${filesizeF}
+*👉🏻Aguarde un momento en lo que envío su audio/video*
 `.trim(), m)
 let _thumb = {}
 try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
 catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
-}
-handler.help = ['play', 'play2'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
+conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+*🔥Título: ${title}*
+`.trim(), m, false, _thumb || {})}
+handler.help = ['play' , 'play2']
 handler.command = /^play2?$/i
-
-handler.exp = 0
-handler.limit = true
-
+handler.group = false
+handler.registrar = false
+handler.limit = false
 module.exports = handler
-
