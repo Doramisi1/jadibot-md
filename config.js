c7a0c876fd559b2727a8c2c4768a754b966fa5d7
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-08-29 18:26:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-global.you = ['5219996125657', '6283170404188', '6283175886677', '6282181661561'] // Developer Bot
-global.rowner = [] // Real Owner Bot
-global.owner = [] // Owner Bot
-global.police = [] // Police Bot
-global.mods = []  // Want some help?
-global.prems = [] // Premium user has unlimited limit
-global.helper = []
+global.you = ['6281368176386','6283840588676','6283802603905','5219996125657', '6283170404188', '6283175886677', '6282181661561'] // Developer Bot
+global.rowner = ['6281368176386','6283840588676','6283802603905'] // Real Owner Bot
+global.owner = ['6281368176386','6283840588676','6283802603905'] // Owner Bot
+global.police = [6281368176386','6283840588676','6283802603905'] // Police Bot
+global.mods = ['6281368176386','6283840588676','6283802603905']  // Want some help?
+global.prems = ['6281368176386','6283840588676','6283802603905'] // Premium user has unlimited limit
+global.helper = ['6281368176386','6283840588676','6283802603905']
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -44,9 +44,9 @@ global.socket = {
 global.useMulti = false //Bug?
 
 // Sticker WM
-global.packname = '3S~Bot'
-global.author = 'ThreeSBot'
-global.wm = '© 3S~Bot'
+global.packname = 'ghost-bot'
+global.author = 'ghost-Bot'
+global.wm = '© ghost👻~Bot'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
