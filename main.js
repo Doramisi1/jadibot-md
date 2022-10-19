fa95fba5ea477f7d1825ce9ae6789ee376a77bef
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-10-20 01:21:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {
   useMultiFileAuthState,
   DisconnectReason, 
   MessageRetryMap, 
-  useSingleFileAuthState
+  useSingleFileAuthState,
 } = require('@adiwajshing/baileys')
 const WebSocket = require('ws')
 const path = require('path')
